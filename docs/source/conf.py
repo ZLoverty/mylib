32e671d11ee656f5b07a98a8d6836ab3f34da85e
Zhengyang Liu <liuzy19911112@gmail.com>
2022-11-14 17:24:01 +0100

    import modules in conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+import corrLib
+import pivLib
+import myImageLib
 
 
 # -- Project information -----------------------------------------------------
