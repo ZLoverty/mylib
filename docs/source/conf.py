42b63ca4302bf089ba2a8436145f1b1d83035ec9
Zhengyang Liu <liuzy19911112@gmail.com>
2023-01-12 14:55:51 +0100

    add press theme to ext list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    "sphinx-press-theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
