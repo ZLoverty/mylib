5c13f4fd000a10755295d3a45029935f4e381e33
Zhengyang Liu <liuzy19911112@gmail.com>
2025-02-28 17:04:26 -0500

    try3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    # "sphinx_press_theme"
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
